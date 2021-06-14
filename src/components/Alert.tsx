70399c23d1c3894165813e5235b8f004c49ac8bf
Matthias Bakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-06-13 21:59:57 -0700

    setup Alert component and styling file for Alert, changed some imports in header and created new svg

---
+++ src/components/Alert.tsx
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+
+import '../styles/Alert.css';
+
+
+const Alert = () => {
+
+}
+
+export default Alert;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import badgerLogo from '../assets/badger_logo.png';
-import leftArrow from '../assets/west_black_24dp.svg';
+import leftArrow from '../assets/arrow_back_black_24dp.svg';
+import rssFeed from '../assets/rss_feed.svg';
 
 import '../styles/Header.css';
 
@@ -15,19 +16,16 @@ const Header = () => {
   }
 
   return (
-    <div className={`${HEADER}container`}>
-      <div className={`${HEADER}page-title`}>
-        <img src={badgerLogo} alt="badger-logo" style={{ height: "50px", width: "50px" }} />
-        <span className={`${HEADER}badger-status`}>
+    <div className={`${HEADER}container`} data-testid={`${HEADER}container`}>
+      <div className={`${HEADER}page-title`} data-testid={`${HEADER}page-title-container`}>
+        <img src={badgerLogo} alt="badger-logo" className={`${HEADER}badger-logo`} data-testid={`${HEADER}badger-logo`} />
+        <span className={`${HEADER}badger-status`} data-testid={`${HEADER}badger-title-link-container`}>
           <h1>Badger Status</h1>
           <a href="https://app.badger.finance/"><img src={leftArrow} alt="left arrow" /> Back to app</a>
         </span>
       </div>
-      <button className={`${HEADER}sub-button`} onClick={() => subscribeHandler()}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 24 24" width="16px" fill="#000000" className={`${HEADER}rss-feed-svg`}>
-          <path d="M0 0h24v24H0V0z" fill="none" /><circle cx="6.18" cy="17.82" r="2.18" />
-          <path d="M4 4.44v2.83c7.03 0 12.73 5.7 12.73 12.73h2.83c0-8.59-6.97-15.56-15.56-15.56zm0 5.66v2.83c3.9 0 7.07 3.17 7.07 7.07h2.83c0-5.47-4.43-9.9-9.9-9.9z" />
-        </svg>SUBSCRIBE TO ALL UPDATES
+      <button className={`${HEADER}sub-button`} onClick={() => subscribeHandler()} data-testid={`${HEADER}subscribe-button`}>
+        <img src={rssFeed} alt="rss-feed-svg" className={`${HEADER}rss-feed-svg`}/>SUBSCRIBE TO ALL UPDATES
       </button>
     </div>
   )
